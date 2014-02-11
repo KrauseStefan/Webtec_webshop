a3991a3efc428c5855d56e88c3f062896f22e87f
Krause <stefan.krausekjaer@gmail.com>
2014-02-11 15:52:02 +0100

    Initial commit of CloudCon and DocumentGenerator

--- AdminModule/src/au/webtech/CloudCon.java
+++ AdminModule/src/au/webtech/CloudCon.java
@@ -1,5 +1,56 @@
 package au.webtech;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.jdom2.Document;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
+
 public class CloudCon {
 
+	public static int sendDocument(HttpURLConnection con, Document doc) throws Exception{
+		DataOutputStream stream = new DataOutputStream(con.getOutputStream());
+		new XMLOutputter().output(doc, stream);
+		stream.flush();
+		stream.close();
+		
+		//Receive response
+		return con.getResponseCode();		
+	}
+
+	public static Document receiveDocument(HttpURLConnection con) throws Exception{
+		return receiveDocument(con, null);
+	}
+	
+	public static Document receiveDocument(HttpURLConnection con, File fileXSD) throws Exception{
+		InputStream input = con.getInputStream();
+				
+		SAXBuilder xmlBuilder = new SAXBuilder();
+//		b.setXMLReaderFactory(XMLReaders.XSDVALIDATING); //TODO Test me
+		xmlBuilder.setValidation(true); 
+
+		if(fileXSD != null){
+			xmlBuilder.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema"); 
+			xmlBuilder.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", fileXSD); 
+		}
+		Document respDoc = xmlBuilder.build(input);
+		input.close();
+		
+		return respDoc;
+	}
+	
+	public static HttpURLConnection createConnection(String url) throws Exception {
+		HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-type", "text/xml");
+		
+		return connection;
+	}
+
+	
 }
--- AdminModule/src/au/webtech/DocumentGenerator.java
+++ AdminModule/src/au/webtech/DocumentGenerator.java
@@ -1,5 +1,65 @@
 package au.webtech;
 
+import java.net.HttpURLConnection;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
 public class DocumentGenerator {
+	private final static String namespaceUrl = "http://www.cs.au.dk/dWebTek/2014";
+	private final static String shopKey = "5247EFB974D2D4D06403F61B";
+	private final static Namespace nsX = Namespace.getNamespace("x", namespaceUrl);
+//	private final static Namespace ns = Namespace.getNamespace("", namespaceUrl);
+	
+	public static Document createItemDocuemnt(String itemName, Namespace ns){
+		Element createItem = new Element("createItem", ns);
+		Document doc = new Document(createItem);
+
+		createItem.addContent((new Element("shopKey", ns)).setText(shopKey));
+		createItem.addContent((new Element("itemName", ns)).setText(itemName));
+		
+		return doc;
+	}
+
+	public static Document modifyItemDocuemnt(Document itemDoc, String itemID){
+		Document _itemDoc = itemDoc.clone();
+		Element root = _itemDoc.getRootElement();
+		root.setName("modifyItem");
+		root.addContent(1, (new Element("shopKey", nsX)).setText(shopKey));
+		root.getChild("itemID", nsX).setText(itemID);
+		root.removeChild("itemStock", nsX);
+				
+		return _itemDoc;	
+	}	
+	
+	public  static Element getItemUsingXpath(Document d, String exp, Namespace ns){
+		XPathFactory factory = XPathFactory.instance();
+		XPathExpression<Element> expression = factory.compile(exp, Filters.element(), null, ns);
+
+		return expression.evaluateFirst(d);
+	}	
+	
+	/**
+	 * Hacked version that assumes no namespace and then adds our namespace.
+	 * 
+	 * @param document
+	 * @param xPathExp
+	 * @return
+	 */
+	public static String getItemValueUsingXpath(Document document, String xPathExp){
+		xPathExp.replaceAll("\\/", "/x:");
+		xPathExp.replaceAll("\\//", "//x:");
+
+		return getItemValueUsingXpath(document, xPathExp, nsX);		
+	}
+	
+	public static String getItemValueUsingXpath(Document document, String xPathExp, Namespace ns){
+		Element element = getItemUsingXpath(document, xPathExp, ns);
+		return element.getTextTrim();	
+	}
 
 }
