2027c2c24a4c4fa292b2ac4cf7ecd2bdfa15b9f3
Thomas Steffensen <T@Thomass-MacBook-Air.local>
2014-02-19 14:08:26 +0100

    added currentItem

---
+++ AdminModule/src/au/webtech/bean/CurrentItem.java
@@ -0,0 +1,10 @@
+package au.webtech.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class CurrentItem extends ShopItem {
+	
+}
