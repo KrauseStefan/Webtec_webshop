687aae71a4fc9dcca6da520a67a826be0792a11a
FireFly3000 <jakobsulby@outlook.dk>
2014-02-25 11:31:10 +0100

    GUI

--- Client/js/wunderShop.js
+++ Client/js/wunderShop.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-	$(window).scroll(function(event) {
-		debugger;
-		$('#floatingHeader');
+	var scrolling = false;
+	
+	$(window).scroll(function() {
+		set = $(document).scrollTop();
+		
+		if(scrolling && set <= 55)
+		{
+			$("#floatingHeader").removeClass("scrolling");
+			scrolling = false;
+		}
+		
+		else if(!scrolling && set > 55)
+		{
+			$("#floatingHeader").addClass("scrolling");
+			scrolling = true;
+		}
 
 	});
 
