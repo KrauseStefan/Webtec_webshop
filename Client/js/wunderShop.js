8866b4ab217ae6a8bd5c8d459e4082b2ce7e7753
Thomas Steffensen <T@Thomass-MacBook-Air.local>
2014-02-25 10:09:53 +0100

    jquery fun

--- Client/js/wunderShop.js
+++ Client/js/wunderShop.js
@@ -1,20 +1,7 @@
 $(document).ready(function() {
-
-	// on window scroll fire it will call a function.
-	$(window).scroll(function() {
-
-		// after window scroll fire it will add define pixel added to that
-		// element.
-		set = $(document).scrollTop() + "px";
-
-		// this is the jQuery animate function to fixed the div position after
-		// scrolling.
-		$('#floatDiv').animate({
-			top : set
-		}, {
-			duration : 1000,
-			queue : false
-		});
+	$(window).scroll(function(event) {
+		debugger;
+		$('#floatingHeader');
 
 	});
 
