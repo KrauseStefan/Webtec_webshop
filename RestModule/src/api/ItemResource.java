9a293a57085a9a01a88e94a20d0443058dc0a2f8
Theis Nickelsen <theisnickelsen@gmail.com>
2014-02-28 11:03:22 +0100

    rest john

--- RestModule/src/api/ItemResource.java
+++ RestModule/src/api/ItemResource.java
@@ -2,13 +2,55 @@ package api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path; 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import au.webtech.*;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 @Path("/service")
 public class ItemResource {
 
 	@GET 
 	@Path("items") 
-	public String GetItems() {
-		return "Hello World!";
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ShopItem> GetItems() throws Exception {
+		List<ShopItem> items = new ArrayList<ShopItem>();
+		HttpURLConnection itemConnection = CloudCon.createConnection(CloudCon.LIST);
+		HttpURLConnection deletedConnection = CloudCon.createConnection(CloudCon.LISTDELETED);
+		Document itemDoc = CloudCon.receiveDocument(itemConnection);
+		Document deletedDoc = CloudCon.receiveDocument(deletedConnection);
+		
+		List<Long> deletedItemIDs = new ArrayList<Long>();
+		
+		Iterator<Element> i = deletedDoc.getRootElement().getChildren().iterator();
+
+		while(i.hasNext()) {			
+			Element element = i.next();
+			
+			deletedItemIDs.add(Long.parseLong(element.getText()));
+		}
+		
+		items.clear();
+	
+		Iterator<Element> it = itemDoc.getRootElement().getChildren().iterator();
+
+		
+		while(it.hasNext()) {			
+			Element element = it.next();
+			ShopItem item = new ShopItem(element);
+			
+			if (!deletedItemIDs.contains(item.getItemID()))
+				items.add(new ShopItem(element));
+		}
+		return items;
+		
 	}
 }
---
+++ RestModule/src/api/ShopItem.java
@@ -0,0 +1,89 @@
+package api;
+
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.jdom2.output.XMLOutputter;
+
+/**
+ * Class is a model of a ShopItem.
+ */
+
+public class ShopItem {
+	public static final String ID = "itemID";
+	public static final String NAME = "itemName";
+	public static final String URL = "itemURL";
+	public static final String PRICE = "itemPrice";
+	public static final String STOCK = "itemStock";
+	public static final String DESCRIPTION = "itemDescription";
+
+	private final static String namespaceUrl = "http://www.cs.au.dk/dWebTek/2014";
+	private final static Namespace ns = Namespace
+			.getNamespace("", namespaceUrl);
+
+	private long itemID;
+	private String itemName;
+	private String itemUrl;
+	private long itemPrice;
+	private long itemStock;
+
+	private Element descElm;
+
+	public ShopItem() {
+
+	}
+
+	public ShopItem(Element element) {
+
+		this.descElm = element.getChild(ShopItem.DESCRIPTION, ns);
+
+		setItemID(Long.parseLong(element.getChildText(ShopItem.ID, ns)));
+		setItemPrice(Long.parseLong(element.getChildText(ShopItem.PRICE, ns)));
+		setItemStock(Long.parseLong(element.getChildText(ShopItem.STOCK, ns)));
+		setItemName(element.getChildText(ShopItem.NAME, ns));
+		setItemUrl(element.getChildText(ShopItem.URL, ns));
+	}
+
+	public long getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(long itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemUrl() {
+		return itemUrl;
+	}
+
+	public void setItemUrl(String itemUrl) {
+		this.itemUrl = itemUrl;
+	}
+
+	public long getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(long itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public long getItemStock() {
+		return itemStock;
+	}
+
+	public void setItemStock(long itemStock) {
+		this.itemStock = itemStock;
+	}
+
+	public String getItemDescription() {
+		return new XMLOutputter().outputElementContentString(descElm);
+	}
+}
