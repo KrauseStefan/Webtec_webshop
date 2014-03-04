62f2e6ee4d3bea78aeb83aa63810be9060132628
STKKJ <stefan.krausekjaer@gmail.com>
2014-03-04 10:58:04 +0100

    Feature: added small webserver to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,81 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jade: {
-			compile: {
-				options: {
-					data: {
-						debug: false
-					},
-					pretty: true,
-				},
-				files: {
-					"app/index.html": ["app/*tmpl.jade"]
-				}
-			}
-		},
-		validation: {
-        	options: {
-                reset: grunt.option('reset') || true,
-                stoponerror: false
-        	},
-        	files: {
-                src: ['app/*.html']
-        	}
-		},
-		sass: {                              	// Task
-			dist: {                            	// Target
-		    	options: {                      // Target options
-		        	style: 'expanded'
-		    	},
-		    	files: {                        // Dictionary of files
-			        'app/mainStyle.css': 'app/mainStyle.tmpl.scss'    // 'destination': 'source'
-		      	}
-		    }
-		},
-		watch: {
-			htmlToIrpt : {
-				files: ['**/*.tmpl.jade'],
-				tasks: ['clear', 'jade', 'sass', 'validation'],
-				options: {
-					livereload: true,
-					spawn: false
-				}
-
-			}
-		}
-
-	});
-	
-    // uglify: {
-      // options: {
-        // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      // },
-      // build: {
-        // src: 'src/<%= pkg.name %>.js',
-        // dest: 'build/<%= pkg.name %>.min.js'
-      // }
-    // }
-  // });
-
-  // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-connect-proxy');
+	// grunt.loadNpmTasks('grunt-connect-rewrite');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-html-validation');
 	grunt.loadNpmTasks('grunt-clear');
-	grunt.loadNpmTasks('grunt-sass');
-  	
-  	// Default task(s).
-	grunt.registerTask('default', ['watch']); //'uglify'
-//	grunt.registerTask('default', ['jade', 'validation']); //'uglify'
+
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// validation: {
+		// 	options: {
+		// 		reset: grunt.option('reset') || true,
+		// 		stoponerror: true
+		// 	},
+		// 	files: {
+		// 		src: ['app/*.html']
+		// 	}
+		// },
+		// watch: {
+		// 	files: ['**/*.tmpl.jade'],
+		// 	tasks: ['clear', 'validation'],
+		// 	options: {
+		// 		livereload: true,
+		// 		spawn: false
+
+		// 	}
+		// },
+		connect: {
+			server: {
+				options: {
+					port: 80,
+					hostname: '0.0.0.0',
+					base: '.',
+					keepalive: true,
+					middleware: function(connect, options) {
+						var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+						// var rewriteRulesSnippet = require('grunt-connect-rewrite/lib/utils').rewriteRequest;
+						// connect.static.mime.define({
+						// 	// 'text/html': ['irpt'],
+						// 	'application/javascript': ['js'],
+						// });
+						return [
+							proxy,
+							// rewriteRulesSnippet,
+							//connect.favicon('images/favicon.ico'),
+							connect.static(options.base),
+							connect.directory(options.base)
+						];
+					}
+				},
+				proxies: [{
+					context: [
+						'/Admin',
+						'/rest'
+					],
+					host: 'localhost',
+					port: 8080,
+					https: false,
+					changeOrigin: true,
+					xforward: true
+				}]
+			}
+			// rules: [{
+			// 	from: '/(.*)$',
+			// 	to: '^/client/WebContent/$1'
+			// }]
+			// rules: [{
+			// 	from: '^/XMII/CM/ArlaPS_SMS_Module/(.*)$',
+			// 	to: '/$1'
+			// }]
+		}
+
+	});
+
+	grunt.registerTask('default', ['configureProxies:server', 'connect']);
 };
