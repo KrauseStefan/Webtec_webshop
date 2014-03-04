74f70800f6e5eb0552351e67e20e68584c3f12c9
FireFly3000 <jakobsulby@outlook.dk>
2014-03-04 12:25:08 +0100

    wunderShop.js

--- client/WebContent/js/wunderShop.js
+++ client/WebContent/js/wunderShop.js
@@ -106,19 +106,67 @@ var loadShoppingBasket = function() {
 	
 };
 
-var loadItems = function() {
-	var url = 
-	
+var loadItems = function() {	
 	$.ajaxSetup({url:"http://localhost:8260/rest/service/items", type:"GET", success:function() {
     		alert( "success" );
 		},
 		error:function() {
 			alert("Error!");
-		},
-		complete:function() {
-			alert("Complete");
 		}
   	});
   
   	$.ajax();
+}
+
+var createTableFromJSON = function(JSON) {
+	var items = JSON.parse(sessionStorage["cart"]);
+	var table = $("#shoppingCartList");
+	
+	table.empty();
+	
+	var row;
+	var cell;
+	var rows = [];
+	
+	function uptateTotal(items){
+		var total = eval(items.map(function(item){return item.itemPrice * item.itemQuantity;}).join('+'))
+		$("#total").text(total)
+	}
+	
+	for(var i = 0; i < items.length; i++){
+		var item = items[i];
+		if(item.itemQuantity <= 0) continue;
+		var row = '<tr>' +
+				'<td>' + item.itemName + '</td>' +
+				'<td class="CartItemPrice">' + item.itemPrice +'</td>' +
+				'<td class="CartItemQuantity">' + item.itemQuantity +'</td>' +
+				'<td> '+
+					'<button class="cartPlusOne">+</button>'+
+					'<button class="cartMinusOne">-</button>'+
+				'</td>' +
+			'</tr>';
+		
+		var row = $($.parseHTML(row));
+		
+		row.find('button').on('click', $.proxy(function(row, item, event){
+			var button = $(event.toElement);
+			if(button.hasClass('cartPlusOne'))
+				item.itemQuantity++;
+			else if(button.hasClass('cartMinusOne'))
+				item.itemQuantity--;
+			
+			window.sessionStorage['cart'] = JSON.stringify(items);
+			uptateTotal(items);
+			if(item.itemQuantity <= 0){
+				row.remove();
+				return;
+			}else{
+			row.find('.CartItemQuantity').text(item.itemQuantity);
+			}
+
+		}, this, row, item));
+		
+		table.append(row);
+	}
+	uptateTotal(items);
 }
